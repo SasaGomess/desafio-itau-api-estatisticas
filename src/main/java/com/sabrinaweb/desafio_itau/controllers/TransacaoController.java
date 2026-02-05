dd476d460855290e04374eab15e5bce1894ff31e
SasaGomes <sabrina.gomess.dev@gmail.com>
2026-02-05 15:09:37 -0300

    feat: add endpoint de deletar uma transação

--- src/main/java/com/sabrinaweb/desafio_itau/controllers/TransacaoController.java
+++ src/main/java/com/sabrinaweb/desafio_itau/controllers/TransacaoController.java
@@ -6,10 +6,7 @@ import com.sabrinaweb.desafio_itau.services.TransacaoService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/transacao")
@@ -25,6 +22,12 @@ public class TransacaoController {
     public ResponseEntity<Void> create(@Valid @RequestBody TransacaoRequest request){
         transacaoService.create(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> delete(){
+        transacaoService.delete();
+        return ResponseEntity.ok().build();
     }
 
 }
